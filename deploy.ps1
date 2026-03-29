d907641af7a5c69c1b5c259eafae566c74d843bd
SemTiOne <emphyst80@gmail.com>
2026-03-29 17:12:56 +0700

    chore: Ko-fi + Resend, auto license delivery in 60s

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,4 @@
-$ErrorActionPreference = "Stop"
-
+(Get-Content .gitignore) -notmatch '^licensing/' | Set-Content .gitignore
 railway up --detach
-
+Copy-Item .gitignore.bak .gitignore
 Write-Host "Deploy done"
